d8885231a31692af917c7f0c38aa4527fade0620
Ohasbini <ohasbini@ethz.ch>
2025-10-26 03:11:27 +0100

    Add files via upload
    load('quasistatic_0001.mat');   % To run identification function
    [alpha0, alpha1] = IdentifyThrottleParams(meas);

---
+++ models/Throttle Parameter Identification.m
@@ -0,0 +1,25 @@
+function [alpha0, alpha1] = IdentifyThrottleParams(data)
+                      
+
+    u_alpha= data.u_alpha.signals.values  ;
+    m_dot_alpha=data.m_dot_alpha.signals.values   ;
+    p_a=data.p_a.signals.values  ;
+    T_a=data.T_a.signals.values   ;
+    R = 287; 
+    y = m_dot_alpha .* sqrt(2 * R .* T_a ./ p_a);
+    M = [ones(length(u_alpha), 1), u_alpha];
+    params = (M' * M) \ (M' * y);
+    alpha0 = params(1);
+    alpha1 = params(2);
+    fprintf('  alpha0 = %.4e [m^2]\n', alpha0);
+    fprintf('  alpha1 = %.4e [m^2]\n', alpha1);
+    A_model = alpha0 + alpha1 * u_alpha;
+    figure; hold on;
+    plot(u_alpha, y, 'kx', 'DisplayName','Measurements');
+    plot(u_alpha, A_model, 'r-', 'LineWidth', 1.5, 'DisplayName','Model Fit');
+    xlabel('Throttle command u_\alpha [%]');
+    ylabel('Equivalent area term');
+    legend('show');
+    grid on;
+    title('Throttle Parameter Identification');
+end
