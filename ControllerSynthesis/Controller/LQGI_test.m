759c74ff3e64ef0ef0957977c0723df397dd289f
khanh ky do <khanhky@outlook.com>
2025-11-27 15:26:38 +0100

    working controller

---
+++ ControllerSynthesis/Controller/LQGI_test.m
@@ -0,0 +1,65 @@
+%run scripts\parameters.m
+
+%Tuning Parameters
+KI = 1; %Larger integral action for larger KI
+r1 = 1;
+r2 = 1;
+
+%Extension Matrices
+Ae = 0;
+Be = [1 0];     % throttle input integrates, ignition does not
+Ce = [KI ;
+      0];
+De = [1 0 ;
+      0 1];
+
+%Compute Extended Plant Matrices
+nx = size(A_lin,1);
+nu = size(B_lin,2);
+
+A = [ A_lin         B_lin*Ce ;
+      zeros(1,nx)   Ae       ];
+B = [ B_lin*De ;
+      Be          ];
+C = [ C_lin   D_lin*Ce ];
+D = D_lin*De;
+
+%Feedback LQR
+
+R = [r1 0;
+     0  r2];
+Q = C' * C;
+K = lqr(A, B, Q, R);
+
+%Observer
+q = 0.01;                      
+L = lqr(A', C', B*B', q)';      
+
+%Matricies
+D_c = zeros(nu, size(C_lin,1));
+ISCS_A_c = [Ae -Be*K;
+       zeros(nx + 1,1)   A - B*K - L*C];
+ISCS_B_c = [Be*D_c;
+       -L];
+ISCS_C_c = [Ce -De*K];
+ISCS_D_c = [De*Dc];
+%Discretize
+sysC = ss(ISCS_A_c, ISCS_B_c, ISCS_C_c, ISCS_D_c);
+sysD = c2d(sysC, 0.001, 'tustin');
+
+ISCS_Ad = sysD.A;
+ISCS_Bd = sysD.B;
+ISCS_Cd = sysD.C;
+ISCS_Dd = sysD.D;
+
+ISCS_Ty = omega_e_nom;                         % output normalization
+ISCS_Tu = diag([u_alpha_nom, du_ign_nom]);     % input normalization
+Ts   = 0.001;
+
+%extension inverter
+s = tf('s');
+Ginv = s/(s + KI);
+
+Ginv_d = c2d(Ginv, Ts, 'tustin');
+[Ainv, Binv, Cinv, Dinv] = ssdata(Ginv_d);
+
